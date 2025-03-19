e78f7ac236d64fc5fc96b9694300b34e4073a1c1
Akhil Chandra <akhilchandra2309@gmail.com>
2025-03-19 22:45:46 +0530

    i18n setup and initial responsive  nav bar addition

---
+++ src/app/components/Buttons/LanguageButton.tsx
@@ -0,0 +1,31 @@
+import { ToggleButton } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import i18n from "../../../../public/Locales/i18n";
+
+const LanguageButton = () => {
+  const [language, setLanguage] = useState("en");
+
+  useEffect(() => {
+    const storedLanguage = localStorage.getItem("language") || "en";
+    setLanguage(storedLanguage);
+    i18n.changeLanguage(storedLanguage);
+  }, []);
+
+  const toggleLanguage = async () => {
+    const newLanguage = language === "en" ? "hi" : "en";
+    localStorage.setItem("language", newLanguage);
+    setLanguage(newLanguage);
+    await i18n.changeLanguage(newLanguage);
+  };
+  return (
+    <ToggleButton
+      value="check"
+      onClick={toggleLanguage}
+      sx={{ borderRadius: "25px", background: "red", padding: "0px 10px" }}
+    >
+      {language == "en" ? "हिन्दी" : "en"}
+    </ToggleButton>
+  );
+};
+
+export default LanguageButton;
